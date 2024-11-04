0f0c71ac7643b60e66506c1c1be6d331d4764c49
tunnu005 <tunnuthe005@gmail.com>
2024-11-04 21:36:04 +0530

    index page

--- src/screens/Index.jsx
+++ src/screens/Index.jsx
@@ -2,16 +2,30 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Video from "../../public/purplebg.mp4";
 import "../css/heart.css";
+
 const Index = () => {
   const navigate = useNavigate();
   const handleNavigate = () => {
     navigate("/signin");
   };
+
   return (
-    <div className="relative ">
-      <video src={Video} className="fixed  w-full" autoPlay muted loop />
-      <div className="h-96 w-[40rem absolute mt-40 ml-9">
-        <h1 id="box" className="text-[#DCBFFF]  w-[35rem] text-left text-2xl font-bold font-sans ">
+    <div className="relative flex flex-col items-center justify-center h-screen bg-gradient-to-br from-purple-700 to-indigo-900 text-white">
+      <div className="absolute inset-0 overflow-hidden">
+        <video
+          src={Video}
+          autoPlay
+          loop
+          muted
+          className="w-full h-full object-cover opacity-50"
+        ></video>
+      </div>
+
+      <div className="relative p-6 bg-black bg-opacity-60 rounded-xl max-w-lg mx-auto text-center lg:max-w-3xl lg:text-left lg:p-12">
+        <h1
+          id="box"
+          className="text-xl font-bold font-sans leading-relaxed lg:text-2xl text-[#DCBFFF]"
+        >
           "Discover the perfect blend of style and simplicity at E Cards, your
           go-to destination for online invitations and event organization. With
           a range of elegant designs and intuitive tools, we make it easy to
@@ -20,7 +34,12 @@ const Index = () => {
           precious memories are always close at hand. Join us and streamline
           your event planning experience effortlessly."
         </h1>
-        <button onClick={handleNavigate} id="bo" className="border-2 mt-9 ml-40 focus-within:border-violet-400 rounded-2xl h-16 w-44 text-white text-lg font-mono hover:bg-gradient-to-br from-purple-900 via-fuchsia-400 to-violet-400">
+
+        <button
+          onClick={handleNavigate}
+          id="bo"
+          className="mt-8 px-8 py-3 bg-gradient-to-br from-purple-900 via-fuchsia-400 to-violet-400 rounded-2xl text-lg font-mono hover:from-purple-700 hover:to-fuchsia-600 transition duration-200 transform hover:scale-105 lg:mt-10"
+        >
           Get Started
         </button>
       </div>
